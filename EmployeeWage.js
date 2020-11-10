a30b8159ca244b51583a34ddc7218882f3d19c2f
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-11-10 23:32:50 +0530

    [Shubham]Added: Ability to add employee information to object

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -130,3 +130,25 @@ empDailyHrsMap.forEach((value,key,map)=>{
 console.log("Full working days: "+fullWorkingDays);
 console.log("Part working days: "+partWorkingDays);
 console.log("Non working days: "+nonWorkingDays);
+
+//UC10 Objects
+{
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArray = new Array();
+    while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random()*10)%3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += getWorkingHours(empCheck);
+        empDailyHrsAndWageArray.push({
+            dayNum : totalWorkingDays,
+            dailyHours:empHrs,
+            dailyWage:calcDailyWage(empHrs),
+            toString() {
+                return "\nDay" + this.dayNum + " => Working Hours is: "+this.dailyHours + " Wage earned = " +this.dailyWage
+            },
+        });
+    }
+    console.log("Printing objects: "+empDailyHrsAndWageArray);
+}
